a35f9fbaf17c94afc2e7920e78fda201e1299bb7
edurelated2021 <edurelated2021@gmail.com>
2025-05-01 18:22:33 +0530

    Add files via upload

--- tools/productRecommendationsService.js
+++ tools/productRecommendationsService.js
@@ -1,1 +1,17 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export function findProductsMatchingKeyWords(inputKeywords) {
+  const dbPath = path.join(__dirname, '../data', 'products.json');
+  const productData = JSON.parse(fs.readFileSync(dbPath, 'utf-8'));
+
+  const matchedProducts = productData.filter(product =>
+    product.keywords.some(keyword => inputKeywords.includes(keyword.toLowerCase()))
+  );
+
+  return matchedProducts;
+}
