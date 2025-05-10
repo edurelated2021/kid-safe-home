c19b55babfa10e5b0dade78decf469460f6802b1
Bahniman <edurelated2021@gmail.com>
2025-05-10 12:09:54 +0530

    Add files via upload

--- js/dashboard.js
+++ js/dashboard.js
@@ -35,7 +35,8 @@ $(document).ready(function() {
     e.preventDefault();
 
     const formData = new FormData(this);
-    $('#photoAnalysisResult').html('<em>Analyzing image...</em>');
+	$('#imageProcessingLoader').show();    
+	$('#photoAnalysisResult').removeClass('visible').html('');
 
     $.ajax({
       type: 'POST',
@@ -44,6 +45,7 @@ $(document).ready(function() {
       processData: false,
       contentType: false,
       success: function (data) {
+		$('#imageProcessingLoader').hide();
         result = data.result.replace(/```html\s*/g, '').replace(/```/g, '');
         $('#photoAnalysisResult').html(result);
         const $img = $('#annotatableImage');
@@ -64,7 +66,8 @@ $(document).ready(function() {
         });
       },
       error: function () {
-        $('#photoAnalysisResult').html('<div class="alert alert-danger">Error analyzing the photo.</div>');
+		$('#imageProcessingLoader').hide();
+        $('#photoAnalysisResult').html('<div class="alert alert-danger">Error analyzing the photo. Please re-upload room image.</div>');
       }
     });
   });
@@ -105,6 +108,7 @@ $(document).ready(function() {
 
     // Show progress indicator
     $('#productLoader').show();
+	$('#productRecommendations').removeClass('visible').html(''); 
 
     // Send query to the server
     $.ajax({
